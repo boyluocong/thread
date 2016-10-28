ce1757f024d0d2d38540a9a86d2e50ad6ea2b99d
luo <luo@boyluo>
2016-10-28 09:49:17 +0800

    测试git

---
+++ src/test/Test.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package test;
+
+/**
+ * @author luo
+ *  @date  2016-10-28
+ */
+public class Test {
+
+	/**
+	 * @author luo
+	 *  @date  2016-10-28
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("≤‚ ‘git");
+	}
+
+}
